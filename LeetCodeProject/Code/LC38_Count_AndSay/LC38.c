4e4e442b2c999327af548e925a997d675a272dd1
Cjm <cjmsaw1113@163.com>
2021-07-18 19:34:47 +0800

    LC38

--- LeetCodeProject/Code/LC38_Count_AndSay/LC38.c
+++ LeetCodeProject/Code/LC38_Count_AndSay/LC38.c
@@ -1,9 +1,60 @@
 #include <stdio.h>
-#include <windows.h>
-int main()
-{
-    printf("Hello World\n");
+#include <stdlib.h>
+#include <string.h>
+char * countAndSay(int n){
+    if ( n == 0 ) {
+        return NULL;
+    }
+    char * pre = (char *)malloc(sizeof(char) * (5000));
+    memset(pre,0,sizeof(char) * (5000));
+    char * target = (char *)malloc(sizeof(char) * (5000));
+    memset(target,0,sizeof(char) * (5000));
+    pre[0] = '1';
+    for (int i = 1;i<n;++i) {
+        int strLen = strlen(pre);
+        if (strLen == 0) {
+            break;
+        }
+        char continuStr = '\0';
+        int indexPre = 0;
+        int num = 0;
+        int indexTar = 0;
+        while (pre[indexPre] != '\0') {
+            if( continuStr == '\0' ) {
+               //表示初始化，碰见第一个元素了,进行记录
+               continuStr = pre[indexPre++]; 
+               num++;
+            }
+            else if (continuStr == pre[indexPre]) {
+                //相等，那么说明该数字连续出现，num自增
+                num++;
+                indexPre++;
+            }
+            else {
+                //不相等，那么就是遇到新的元素了，更新continuStr，并计算当前已经弄好的字符串
+                // printf("%d,%c\n",num,continuStr);
+                target[indexTar++] = num + '0';
+                target[indexTar++] = continuStr;
+                continuStr = pre[indexPre++]; 
+                num = 1;
+            }
+        }
+        // printf("%d,%c\n",num,continuStr);
+        //下面此部分是为末尾元素进行控制
+        target[indexTar++] = num + '0';
+        target[indexTar++] = continuStr;
+        strcpy(pre,target);
+    }
+    free(target);
+    return pre;
+}
+int main (void) {
+    int n = 0;
+    printf("Please enter number between 0~20: ");
+    scanf("%d",&n);
+    char * res = countAndSay(n);
+    printf("result = %s\n",res);
+    free(res);
     system("pause");
-    int res;
     return 0;
 }
